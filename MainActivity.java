68da984ece9658e191c8bce1712cb59780fcb24b
Aswathy Chandran <125429061+aswathy-235@users.noreply.github.com>
2023-10-10 09:34:18 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.sjcet.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+    }
+
+    public void change(View view) {
+        tv.setText("ASWATHY" +
+                "");
+    }
+}
